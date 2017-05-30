78bf1f550b6146f1e4a20c660cf045002822699e
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2017-05-30 17:42:03 -0300

    removing old project

--- projects/simple_counter2/apply_commands.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-source $THIS_DIR/../../tools/defaults.sh
-
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-$CLI_PATH $THIS_DIR/build/simple_counter.json < $THIS_DIR/commands.txt
